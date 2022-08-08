9a4f76dc017aaf6b65aede4fcc85952f0410a35d
thereal-atom <oscarfal2006@gmail.com>
2022-08-09 00:19:00 +0100

    bold markdown for messages

--- src/components/MarkdownContent.svelte
+++ src/components/MarkdownContent.svelte
@@ -1,9 +1,31 @@
 <script lang="ts">
     export let string: string;
+
+    /**
+     * DONE: \n (new line)
+     * DONE: **string** (bold)
+     * TODO ~~string~~ (strike through)
+     * TODO `code` (code)
+     * TODO ```code block``` (code block)
+     * TODO [string](url) (hyperlink)
+     * TODO _string_ (italic)
+     * TODO __string__ (underline)
+     * TODO ___string___ (monospace)
+     * TODO ??string?? (upside down)
+     * TODO > string (quote) 
+     * TODO ||string|| (hidden)
+    */
 </script>
 
-<div class="flex flex-col">
-    {#each string.split("\n") as line, i}
+<div class="flex flex-row">
+    <!-- {#each string.split("\n") as line, i}
         <p class="h-6">{line}</p>
+    {/each} -->
+    {#each string.split("**") as line, i}
+        {#if i % 2 === 0}
+            {line}
+        {:else}
+            <span class="font-black mx-1">{line}</span>
+        {/if}
     {/each}
 </div>
