ceec42a11c66ef7ce74eb1a400a9130371628090
thereal-atom <oscarfal2006@gmail.com>
2022-08-11 23:30:40 +0100

    file upload button

--- src/routes/dashboard/organization/[organizationId]/files/__layout.svelte
+++ src/routes/dashboard/organization/[organizationId]/files/__layout.svelte
@@ -1,12 +1,45 @@
-<script>
+<script lang="ts">
     import { page } from "$app/stores";
     import "../../../../../styles/dashboard/files.css";
 
     let { organizationId } = $page.params;
+
+    const handleFileUpload = (files: FileList) => {
+        // @ts-ignore
+        for (const file of files) {
+            console.log(file.name);
+        };
+    };
+
+    let files: FileList;
+    $: files;
+
+    $: if(files?.length > 0) handleFileUpload(files);
 </script>
 
 <div class="flex flex-row w-full h-full">
-    <slot />
+    <div class="relative w-full">
+        <slot />
+        <input
+            type="file"
+            bind:files={files}
+            class="hidden"
+            id="file-uploader"
+            accept="image/*"
+            multiple
+        />
+        <div
+            class="absolute flex items-center justify-center rounded-full w-12 h-12 bg-accent right-8 bottom-8 text-3xl font-bold hover:cursor-pointer"
+            on:click={() => {
+                const fileUploader = document.getElementById("file-uploader");
+                if (!fileUploader) return;
+                
+                fileUploader.click();
+            }}
+        >
+            +
+        </div>
+    </div>
     <div class="h-full w-3xx flex flex-col border border-solid border-dark-primary border-y-0 border-r-0 p-8">
         <span class="sidebar-title">Media</span>
         <a class="sidebar-link" href="/dashboard/organization/{organizationId}/files/logos">Logos</a>
