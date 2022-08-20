e5f74a568ef060fb9cc20afa3bba511216d6bf11
thereal-atom <oscarfal2006@gmail.com>
2022-08-20 10:53:35 +0100

    fixed overflow issues with mobile nav menu

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,13 @@
 <script>
+    import { onMount } from "svelte";
     import "../app.css";
+    
+    onMount(() => {
+        // @ts-ignore
+        document.querySelector("body").classList.remove("overflow-hidden");
+        // @ts-ignore
+        document.querySelector("html").classList.remove("overflow-hidden");
+    });
 </script>
 
 <slot />
