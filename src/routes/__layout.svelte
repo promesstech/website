840f64657ce104f914691eb60ca791e6258714e6
thereal-atom <oscarfal2006@gmail.com>
2022-08-22 10:49:48 +0100

    fixed fouc

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,6 +7,18 @@
         document.querySelector("body").classList.remove("overflow-hidden");
         // @ts-ignore
         document.querySelector("html").classList.remove("overflow-hidden");
+
+        // @ts-ignore
+        let domReady = (cb) => {
+            document.readyState === 'interactive' || document.readyState === 'complete'
+                ? cb()
+                : document.addEventListener('DOMContentLoaded', cb);
+        };
+
+        domReady(() => {
+            // Display body when DOM is loaded
+            document.body.style.visibility = 'visible';
+        });
     });
 </script>
 
